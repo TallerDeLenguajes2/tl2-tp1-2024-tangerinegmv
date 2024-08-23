451e295de1b00b40be137f11d87a65e407a8f424
Victoria <gimenezmariavictoria95@gmail.com>
2024-08-23 19:45:52 -0300

    aqui explico el diseño del código, tiene que ver con la composicion y agregacion

--- Program.cs
+++ Program.cs
@@ -9,10 +9,20 @@ namespace EspacioCadeteria
     {
         //private int numero;
         public string obs;
-        private List<Cliente> clientes;
+        private Cliente cliente;
+
+        public Pedidos(string nombre, string direccion, int tel, string DRD)//esto se hace asi porque la consigna no nos pide una lista de clientes, sino que cuando llega un pedido nace un cliente, y cuando se cancela o despacha un pedido el cliente se va, por eso no  creo un clientte antes, sino directamente en la clase pedidos
+        {
+            this.cliente = new Cliente(){
+                Nombre = nombre,
+                Telefono = tel,
+                Direccion = direccion,
+                DatosReferenciaDireccion = DRD
+            };
+            
+        }
 
         public int Numero { get ; set ; }//se puede hacer esto y no haría falta la linea 3
-        public List<Cliente> Clientes { get => clientes; set => clientes = value; }
         public Estados Estado { get; set; }
     }
 
@@ -21,7 +31,15 @@ namespace EspacioCadeteria
         private string nombre;
         private string direccion;
         private int telefono;
-        
+        public string DatosReferenciaDireccion;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public int Telefono { get => telefono; set => telefono = value; }
     }
+    
+
+    
     //en cadete hago el constructor cadete(){new list PEdido}
 }//en cadete metodo agregarpedido y quitarpedido
+//en constructor crear Constructor en clase pedidos de Clientes cliente, con parametros los campos de cliente
