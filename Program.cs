a086c44a2a94e5b1f3a78b0309816c46dac34413
Victoria <gimenezmariavictoria95@gmail.com>
2024-08-23 19:25:39 -0300

    empezando a armar las clases

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//comando Split para archivo csv
+namespace EspacioCadeteria
+{
+    public enum Estados
+    {
+        
+    }
+    public class Pedidos
+    {
+        //private int numero;
+        public string obs;
+        private List<Cliente> clientes;
+
+        public int Numero { get ; set ; }//se puede hacer esto y no haría falta la linea 3
+        public List<Cliente> Clientes { get => clientes; set => clientes = value; }
+        public Estados Estado { get; set; }
+    }
+
+    public class Cliente
+    {
+        private string nombre;
+        private string direccion;
+        private int telefono;
+        
+    }
+    //en cadete hago el constructor cadete(){new list PEdido}
+}//en cadete metodo agregarpedido y quitarpedido
